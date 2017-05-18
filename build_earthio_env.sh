c29097309615897db07aa638ce6772d07251c8a8
Peter Steinberg <psteinberg@continuum.io>
2017-05-18 16:42:18 -0700

    fix travis env settings

--- build_earthio_env.sh
+++ build_earthio_env.sh
@@ -1,5 +1,9 @@
 export EARTHIO_TEST_ENV=earth-env-test
 build_earthio_env(){
+    if [ "$PYTHON_TEST_VERSION" = "" ];then
+        echo Env variable PYTHON_TEST_VERSION is not defined. Set it to 2.7, 3.5 or 3.6 - FAIL ;
+        return 1;
+    fi
     export BUILD_DIR=`pwd -P`
     if [ "$MAKE_MINICONDA" = "" ];then
         echo
@@ -12,18 +16,19 @@ build_earthio_env(){
     conda install --name root conda conda-build;
     conda env remove --name ${EARTHIO_TEST_ENV} &> /dev/null;
     conda config --set always_yes true  || return 1;
-    conda env create -n ${EARTHIO_TEST_ENV} -f environment.yml  || return 1;
     source activate ${EARTHIO_TEST_ENV}  || return 1;
     if [ "$EARTHIO_INSTALL_METHOD" = "" ];then
         export EARTHIO_INSTALL_METHOD="conda";
     fi
     if [ "$EARTHIO_INSTALL_METHOD" = "git" ];then
+        conda env create -n ${EARTHIO_TEST_ENV} -f environment.yml  || return 1;
         python setup.py develop || return 1;
     else
-        conda build -c conda-forge --python $EARTH_VERS conda.recipe || return 1;
+        conda build -c conda-forge --python $PYTHON_TEST_VERSION conda.recipe || return 1;
+        conda create --use-local --name $EARTHIO_TEST_ENV python=$PYTHON_TEST_VERSION earthio || return 1;
     fi
     export ELM_EXAMPLE_DATA_PATH=$(pwd -P)/elm-data
-    conda install -c defaults -c conda-forge requests pbzip2 python-magic six  || return 1;# for download_test_data.py
+    conda install -c defaults -c conda-forge requests pbzip2 python-magic six  || return 1; # for download_test_data.py
     mkdir -p $ELM_EXAMPLE_DATA_PATH && cd $ELM_EXAMPLE_DATA_PATH && python "${BUILD_DIR}/scripts/download_test_data.py"  || return 1;
     cd $BUILD_DIR
 }
