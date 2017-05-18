1ad9a17e0a9afe9c85a95c9c4655608a6e9fc1d7
Peter Steinberg <psteinberg@continuum.io>
2017-05-18 15:59:44 -0700

    fix some travis issues

--- build_earthio_env.sh
+++ build_earthio_env.sh
@@ -1,31 +1,30 @@
-export BUILD_DIR=`pwd -P`
-if [ "$MAKE_MINICONDA" = "" ];then
-    echo
-else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    bash miniconda.sh -b -p $HOME/miniconda
-    export PATH="$HOME/miniconda/bin:$PATH"
-fi
-source deactivate
-export env_=earth-env-test
-conda env remove --name ${env_} &> /dev/null
-conda config --set always_yes true
-conda env update -n ${env_} -f environment.yml
-source activate ${env_}
-python setup.py develop
-export ELM_EXAMPLE_DATA_PATH=$(pwd -P)/elm-data
-conda install -c defaults -c conda-forge requests pbzip2 python-magic six # for download_test_data.py
-mkdir -p $ELM_EXAMPLE_DATA_PATH && cd $ELM_EXAMPLE_DATA_PATH && python "${BUILD_DIR}/scripts/download_test_data.py"
-
-# TODO remove this following line once data
-# downloader for Tiffs is okay
-rm -rf tif
-python -c 'from earthio.s3_landsat_util import SceneDownloader;s = SceneDownloader();row = s.lowest_cloud_cover_image();s.download_all_bands(row.download_url.iloc[0])'
-ls -lRth ./
-pwd
-echo ELM_EXAMPLE_DATA_PATH is $ELM_EXAMPLE_DATA_PATH
-## End of section that needs to be removed soon
-cd $BUILD_DIR
-ls -lRth $ELM_EXAMPLE_DATA_PATH
-
+export EARTHIO_TEST_ENV=earth-env-test
+build_earthio_env(){
+    export BUILD_DIR=`pwd -P`
+    if [ "$MAKE_MINICONDA" = "" ];then
+        echo
+    else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || return 1;
+        bash miniconda.sh -b -p $HOME/miniconda || return 1;
+        export PATH="$HOME/miniconda/bin:$PATH" || return 1;
+    fi
+    source deactivate  || return 1;
+    conda env remove --name ${EARTHIO_TEST_ENV} &> /dev/null;
+    conda config --set always_yes true  || return 1;
+    conda env create -n ${EARTHIO_TEST_ENV} -f environment.yml  || return 1;
+    source activate ${EARTHIO_TEST_ENV}  || return 1;
+    if [ "$EARTHIO_INSTALL_METHOD" = "" ];then
+        export EARTHIO_INSTALL_METHOD="conda";
+    fi
+    if [ "$EARTHIO_INSTALL_METHOD" = "git" ];then
+        python setup.py develop || return 1;
+    else
+        conda build -c conda-forge --python $EARTH_VERS conda.recipe || return 1;
+    fi
+    export ELM_EXAMPLE_DATA_PATH=$(pwd -P)/elm-data
+    conda install -c defaults -c conda-forge requests pbzip2 python-magic six  || return 1;# for download_test_data.py
+    mkdir -p $ELM_EXAMPLE_DATA_PATH && cd $ELM_EXAMPLE_DATA_PATH && python "${BUILD_DIR}/scripts/download_test_data.py"  || return 1;
+    cd $BUILD_DIR
+}
+build_earthio_env && source activate ${EARTHIO_TEST_ENV} && echo OK
 
