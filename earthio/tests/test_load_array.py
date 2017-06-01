3c5341c64789e046e70d6133f80cd82c0004cd22
gbrener <gbrener@continuum.io>
2017-06-01 11:56:59 -0500

    Update tests to remove hard-coded datatype dependencies

--- earthio/tests/test_load_array.py
+++ earthio/tests/test_load_array.py
@@ -7,10 +7,16 @@ from earthio.tests.util import (TIF_FILES, HDF5_FILES,
                                     HDF4_FILES, NETCDF_FILES,
                                     ELM_HAS_EXAMPLES)
 from earthio.tests.test_tif import band_specs as tif_band_specs
-TRIALS = {'tif': os.path.dirname(TIF_FILES[0]) if TIF_FILES else 'tif',
-          'hdf5': HDF5_FILES[0] if HDF5_FILES else 'hdf5',
-          'hdf4': HDF4_FILES[0] if HDF4_FILES else 'hdf4',
-          'netcdf': NETCDF_FILES[0] if NETCDF_FILES else 'netcdf'}
+
+TRIALS = {}
+if TIF_FILES:
+    TRIALS['tif'] = os.path.dirname(TIF_FILES[0])
+if HDF5_FILES:
+    TRIALS['hdf5'] = HDF5_FILES[0]
+if HDF4_FILES:
+    TRIALS['hdf4'] = HDF4_FILES[0]
+if NETCDF_FILES:
+    TRIALS['netcdf'] = NETCDF_FILES[0]
 
 @pytest.mark.skipif(not ELM_HAS_EXAMPLES,
                reason='elm-data repo has not been cloned')
