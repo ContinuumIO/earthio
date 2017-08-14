87ec28be7d76871bf11ad6a335435c4f3d2698be
gbrener <gbrener@continuum.io>
2017-08-14 10:06:53 -0500

    Get travis-ci to pass

--- build_earthio_env.sh
+++ build_earthio_env.sh
@@ -1,45 +1,43 @@
 source deactivate;
 
 export EARTHIO_BUILD_DIR=`pwd -P`
-export EARTHIO_CHANNEL_STR="${EARTHIO_CHANNEL_STR:- -c ioam -c conda-forge -c scitools/label/dev }"
+export EARTHIO_CHANNEL_STR="${EARTHIO_CHANNEL_STR:- -c ioam -c conda-forge -c scitools/label/dev -c elm }"
 export EARTHIO_TEST_ENV="${EARTHIO_TEST_ENV:-earth-env-test}"
 export EARTHIO_INSTALL_METHOD="${EARTHIO_INSTALL_METHOD:-conda}"
 export ELM_EXAMPLE_DATA_PATH="${ELM_EXAMPLE_DATA_PATH:-${EARTHIO_BUILD_DIR}/../elm-data}";
 export PYTHON=${PYTHON:-3.5}
 export NUMPY=${NUMPY:-1.11}
 
-build_earthio_env() {
-    set -e
+set -e
 
-    if [ -n "$MAKE_MINICONDA" ];then
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-        bash miniconda.sh -b -p $HOME/miniconda
-        export PATH="$HOME/miniconda/bin:$PATH"
-    fi
+if [ -n "$MAKE_MINICONDA" ];then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -f -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+fi
 
-    conda config --set always_yes true;
-    conda install --name root conda conda-build;
-    conda env remove --name ${EARTHIO_TEST_ENV} &> /dev/null;
+conda config --set always_yes true
+conda install --name root conda conda-build
+conda env remove --name ${EARTHIO_TEST_ENV} &> /dev/null
 
-    if [ "x$EARTHIO_INSTALL_METHOD" = "xgit" ];then
-        conda env create -n ${EARTHIO_TEST_ENV} -f environment.yml
-        source activate ${EARTHIO_TEST_ENV}
-        python setup.py develop
-    else
-        conda build $EARTHIO_CHANNEL_STR --python $PYTHON --numpy $NUMPY conda.recipe
-        conda create --use-local --name $EARTHIO_TEST_ENV $EARTHIO_CHANNEL_STR python=$PYTHON numpy=$NUMPY earthio
-        source activate ${EARTHIO_TEST_ENV}
-    fi
+if [ "x$EARTHIO_INSTALL_METHOD" = "xgit" ];then
+    conda env create -n ${EARTHIO_TEST_ENV} -f environment.yml
+    source activate ${EARTHIO_TEST_ENV}
+    python setup.py develop
+else
+    conda build $EARTHIO_CHANNEL_STR --python $PYTHON --numpy $NUMPY conda.recipe
+    conda create --use-local --name $EARTHIO_TEST_ENV $EARTHIO_CHANNEL_STR python=$PYTHON numpy=$NUMPY earthio
+    source activate ${EARTHIO_TEST_ENV}
+fi
 
-    if [ "x$IGNORE_ELM_DATA_DOWNLOAD" = "x" ];then
-        conda install -c defaults -c conda-forge requests pbzip2 python-magic
-        mkdir -p $ELM_EXAMPLE_DATA_PATH
-        df -h
-        pushd $ELM_EXAMPLE_DATA_PATH && python "${EARTHIO_BUILD_DIR}/scripts/download_test_data.py" --files hdf4.tar.bz2 tif.tar.bz2  && popd
-        df -h
-    fi
+if [ "x$IGNORE_ELM_DATA_DOWNLOAD" = "x" ];then
+    conda install -c defaults -c conda-forge requests pbzip2 python-magic
+    mkdir -p $ELM_EXAMPLE_DATA_PATH
+    df -h
+    pushd $ELM_EXAMPLE_DATA_PATH && python "${EARTHIO_BUILD_DIR}/scripts/download_test_data.py" --files hdf4.tar.bz2 tif.tar.bz2  && popd
+    df -h
+fi
 
-    set +e
-}
+set +e
 
-build_earthio_env && source activate ${EARTHIO_TEST_ENV} && echo OK
+source activate ${EARTHIO_TEST_ENV} && echo OK
